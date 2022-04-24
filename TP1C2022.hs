f0176e7c2aa1a56e8ca16a8b1e0e94738c29c045
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-04-24 17:37:23 -0300

    Hicimos el 2. Pendientes el 3 y 4

--- TP1C2022.hs
+++ TP1C2022.hs
@@ -7,11 +7,12 @@ hacerCero :: Integer -> Integer
 hacerCero x | x == 0 = 0 
                            | otherwise = hacerCero (x-1)
 
---sequenciaCollatz :: Integer ->  Integer
+sequenciaCollatz :: Integer ->  Bool
 
---sequenciaCollatz n | n == 1 = 1
-                                            -- | esPar n == True =  sequenciaCollatz(n `div` 2)
-                                            -- | otherwise = sequenciaCollatz(3 * n + 1 )
+sequenciaCollatz n | n == 1 = True
+                                            | esPar n == True =  sequenciaCollatz(n `div` 2)
+                                            | esPar n == False = sequenciaCollatz(3 * n + 1 )
+				    | otherwise = False
                      
                     
 sequenciaCollatz2 n       | esPar n = n `div` 2
@@ -30,11 +31,45 @@ kEsimoCollatz  n k    | esPar n == True = kEsimoCollatz(n `div` 2) (k-1)
 
 satisfaceCollatz :: Integer -> Integer -> Bool
 
-satisfaceCollatz 1 1 = False
-satisfaceCollatz 1 2 = False
-satisfaceCollatz 1 3 = False
+--satisfaceCollatz 1 1 = False
+--satisfaceCollatz 1 2 = False
+--satisfaceCollatz 1 3 = False
 satisfaceCollatz n m  | kEsimoCollatz n m == 1 = True
 				         | otherwise = False
 						 
 --Termina 1
+
 --Empieza 2
+
+satisfaceCollatzHasta :: Integer -> Integer -> Bool
+satisfaceCollatzHasta 0 m = True
+satisfaceCollatzHasta n m|satisfaceCollatz n m == True = satisfaceCollatzHasta (n-1) m 
+                                                            | otherwise = False
+															
+
+--Termina 2
+
+--Empieza 3
+
+--kEsimoCollatzPar :: Integer -> Integer -> Integer
+
+--kEsimoCollatzPar n 0 =  n
+--kEsimoCollatzPar 1 k = 1
+--kEsimoCollatzPar  n k  | esPar n == True = kEsimoCollatz(n `div` 2) (k-1)
+                                               --   | otherwise = kEsimoCollatz(3 * n + 1) (k-1) 
+
+
+
+--cantidadTerminosPares :: Integer -> Integer
+--cantidadTerminosPares n | sequenciaCollatz n == False = 0
+                                                        -- |  esPar n == True = 
+														 
+--TERMINAR 3
+
+
+--Empieza 4
+
+largoSecuencia :: Integer -> Integer
+
+largoSecuencia 1 = 0
+largoSecuencia n | kEsimoCollatz n n == 1 = n 
