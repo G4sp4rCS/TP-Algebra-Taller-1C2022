45a997bb90b5e5f1bc72b80384d3a9fd2107240f
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-04-25 21:40:48 -0300

    Agregue comentarios explicativos. Empecé el 3

--- TP1C2022.hs
+++ TP1C2022.hs
@@ -1,12 +1,11 @@
+--VERIFICA SI EL NUMERO ES PAR
 esPar :: Integer -> Bool
 esPar n | n == 0 = True
                  | n == 1 = False
                  | otherwise = esPar(n-2)
 
-hacerCero :: Integer -> Integer              
-hacerCero x | x == 0 = 0 
-                           | otherwise = hacerCero (x-1)
 
+--VERIFICA SI CUMPLE LA CONJETURA DE COLLATZ
 sequenciaCollatz :: Integer ->  Bool
 
 sequenciaCollatz n | n == 1 = True
@@ -14,32 +13,29 @@ sequenciaCollatz n | n == 1 = True
                                             | esPar n == False = sequenciaCollatz(3 * n + 1 )
 				    | otherwise = False
                      
-                    
-sequenciaCollatz2 n       | esPar n = n `div` 2
-                                                      | otherwise = 3 * n + 1
+                                        
                      
-                     
-                
+ --CALCULA EL TERMINO k-ESIMO DE LA SUCESION DE COLLATZ DE UN NUMERO              
 kEsimoCollatz :: Integer -> Integer -> Integer
 
-kEsimoCollatz n 0 =  n
+kEsimoCollatz n 0 =  n 
 kEsimoCollatz 1 k = 1
 kEsimoCollatz  n k    | esPar n == True = kEsimoCollatz(n `div` 2) (k-1)
                                             | otherwise = kEsimoCollatz(3 * n + 1) (k-1)                                         
                               
 
+--EJERCICIO 1
 
 satisfaceCollatz :: Integer -> Integer -> Bool
 
---satisfaceCollatz 1 1 = False
---satisfaceCollatz 1 2 = False
---satisfaceCollatz 1 3 = False
 satisfaceCollatz n m  | kEsimoCollatz n m == 1 = True
 				         | otherwise = False
 						 
 --Termina 1
 
 --Empieza 2
+
+--EJERCICIO 2
 
 satisfaceCollatzHasta :: Integer -> Integer -> Bool
 satisfaceCollatzHasta 0 m = True
@@ -51,25 +47,33 @@ satisfaceCollatzHasta n m|satisfaceCollatz n m == True = satisfaceCollatzHasta (
 
 --Empieza 3
 
---kEsimoCollatzPar :: Integer -> Integer -> Integer
 
---kEsimoCollatzPar n 0 =  n
---kEsimoCollatzPar 1 k = 1
---kEsimoCollatzPar  n k  | esPar n == True = kEsimoCollatz(n `div` 2) (k-1)
-                                               --   | otherwise = kEsimoCollatz(3 * n + 1) (k-1) 
+cantTerminosParesDesde n 0 = 0
+cantTerminosParesDesde n t | esPar(kEsimoCollatz n t) == True = 1 + cantTerminosParesDesde n (t-1)  
+						   | otherwise = cantTerminosParesDesde n(t-1)  
 
 
-
---cantidadTerminosPares :: Integer -> Integer
---cantidadTerminosPares n | sequenciaCollatz n == False = 0
-                                                        -- |  esPar n == True = 
-														 
+cantidadTerminosPares :: Integer -> Integer
+cantidadTerminosPares 0 = 1
+cantidadTerminosPares n = cantTerminosParesDesde n n 
+                                                    													 
 --TERMINAR 3
 
 
 --Empieza 4
 
-largoSecuencia :: Integer -> Integer
+--largoSecuencia :: Integer -> Integer
 
-largoSecuencia 1 = 0
-largoSecuencia n | kEsimoCollatz n n == 1 = n 
+--largoSecuencia 1 = 0
+--largoSecuencia n | kEsimoCollatz n  == 1 = n 
+                                        
+
+														 
+														 
+														 
+														 
+
+
+
+
+
